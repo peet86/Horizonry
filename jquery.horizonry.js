49b2e055eb479b4b600a9d27f133830ccb191832
Peter Varga <admin@vargapeter.com>
2015-02-23 17:45:43 +0100

    Update jquery.horizonry.js

--- jquery.horizonry.js
+++ jquery.horizonry.js
@@ -1,131 +1,133 @@
 /*!
  * Horizonry v1.0
- * CSS3 Masonry Horizontal Ordering 
+ * CSS3 Masonry Horizontal Ordering
  * http://vargapeter.com
  * MIT License
  * by Peter Varga (vargapeter.com)
  */
-(function($){
-    $.horizonry = function(el, options){
+(function ($) {
+    $.horizonry = function (el, options) {
         var base = this;
-        
+
         base.$el = $(el);
         base.el = el;
-      
-      	base.columns=0;
-      	      
-      	base.cols=[];  
-      
-        base.$el.data("horizonry", base);
-        
-        base.init = function(){
-            base.options = $.extend({},$.horizonry.defaultOptions, options);
-          
- 						base.$children=	base.$el.children("."+base.options.itemSelector);
-          
-          	base.$children_first=base.$children.first();
-                    
-            base.window_width=$(window).width();  
-              
-          	// initial sort
-            base.sort();
-        };
-      
-      	// resize listener
-      	$( window ).resize(function(e) {
-          		// horizontal resize only
-            	if ($(window).width()==base.window_width) return; 
-  						base.window_width = $(window).width();
-          	
-          		// delay 500ms
-          		if(this.resizeTO) clearTimeout(this.resizeTO);
-              this.resizeTO = setTimeout(function() {
-                $(this).trigger('resizeEnd');
-              }, 500);
-        });
-      
-      	// resize end listener
-      	$(window).bind('resizeEnd', function() {
-            console.log("resize");
-          	base.reset();
-            base.sort();
-        });
-      
-      	base.getCols = function($this){
-          var containerwidth=base.$el.width(),
-              itemwidth=base.$children_first.width(),
-          		pos=base.$children_first.offset(),
-              totalwidth=pos.left+itemwidth;
-          return Math.round(containerwidth/totalwidth);
-        };
-      
-     
-      	// first in a column
-      	base.moveFirst = function($this){
-          $this.addClass(base.options.firstItemSelector);
-          base.cols.push($this.height());
-          console.log("f");
-        }
-      
-      	// basic masonry logic (min. height column)
-      	base.moveMasonry = function($this){
-          // min height col
-          var minh=Math.min.apply(Math, base.cols),
-              mincol=base.cols.indexOf(minh);
 
-          // if last column append to the end
-          if((mincol+1)==base.cols.length){
-            base.$el.append($this);
-          // insert before the next "first item"
-          }else{
-            var bfselector="."+base.options.firstItemSelector+":eq("+(mincol+1)+")",
-                bf=base.$el.find(bfselector);
-            $this.insertBefore(bf[0]);                  
-          }        
-          base.cols[mincol]+=$this.height();
+        base.columns = 0;
+
+        base.cols = [];
+
+        base.$el.data("horizonry", base);
+
+        base.init = function () {
+            base.options = $.extend({}, $.horizonry.defaultOptions, options);
+
+            base.$children = base.$el.children("." + base.options.itemSelector);
+
+            base.$children_first = base.$children.first();
+
+            base.window_width = $(window).width();
+
+            // initial sort
+            base.sort();
         };
-      
-      	base.reset = function($children){
-          base.cols=[];
-          base.$children.removeClass(base.options.firstItemSelector);
-          base.$children.sort(function (a, b) {
-              return +a.dataset.horizonrypos - +b.dataset.horizonrypos;
-          })
-          .appendTo(base.$el);
+
+        // resize listener
+        $(window).resize(function (e) {
+            // horizontal resize only
+            if ($(window).width() == base.window_width) return;
+            base.window_width = $(window).width();
+
+            // delay 500ms
+            if (this.resizeTo) clearTimeout(this.resizeTo);
+
+            this.resizeTo = setTimeout(function () {
+                $(this).trigger('resizeEnd');
+            }, 500);
+        });
+
+        // resize end listener
+        $(window).bind('resizeEnd', function () {
+            base.reset();
+            base.sort();
+        });
+
+        base.getCols = function ($this) {
+            var containerwidth = base.$el.width(),
+                itemwidth = base.$children_first.width(),
+                pos = base.$children_first.offset(),
+                totalwidth = pos.left + itemwidth;
+
+            var r = Math.round(containerwidth / totalwidth);
+            return r;
         };
-      
-      	base.sort = function(){
-          	// get possible column number
-            base.columns=base.getCols();
-   
-          	// get the children
-          	base.$children.each(function(i){
-              var $this=$(this);
-							
-              // set original position for later
-              $this.data("horizonrypos",i);
-                            
-              // if the column will be visible 
-              if(i<=(base.columns-1)){                  
-                base.moveFirst($this);
-              }else{
-              	base.moveMasonry($this);
-              }           
-           });
+
+
+        // first in a column
+        base.moveFirst = function ($this) {
+            $this.addClass(base.options.firstItemSelector);
+            base.cols.push($this.height());
         }
-      
+
+        // basic masonry logic (min. height column)
+        base.moveMasonry = function ($this) {
+            // min height col
+            var minh = Math.min.apply(Math, base.cols),
+                mincol = base.cols.indexOf(minh);
+
+            // if last column append to the end
+            if ((mincol + 1) == base.cols.length) {
+                base.$el.append($this);
+                // insert before the next "first item"
+            } else {
+                var bfselector = "." + base.options.firstItemSelector + ":eq(" + (mincol + 1) + ")",
+                    bf = base.$el.find(bfselector);
+                $this.insertBefore(bf[0]);
+            }
+            base.cols[mincol] += $this.height();
+        };
+
+        // reset order
+        base.reset = function ($children) {
+            base.cols = [];
+            base.$children.removeClass(base.options.firstItemSelector);
+            base.$el.append(base.$children.sort(function (a, b) {
+                return $(a).attr("data-horizonrypos") - $(b).attr("data-horizonrypos");
+            }));
+        };
+
+        base.sort = function () {
+            // get possible column number
+            base.columns = base.getCols();
+
+            // get the children
+            base.$children.each(function (i) {
+                var $this = $(this);
+
+                // set original position for later
+                $this.attr("data-horizonrypos", i);
+
+                // if the column will be visible 
+                if (i <= (base.columns - 1)) {
+                    base.moveFirst($this);
+                } else {
+                    base.moveMasonry($this);
+
+                }
+            });
+        }
+
         base.init();
     };
-    
+
     $.horizonry.defaultOptions = {
-      itemSelector: "item", 
-      firstItemSelector: "item-first",
+        itemSelector: "item",
+        firstItemSelector: "item-first",
     };
-    
-    $.fn.horizonry = function(options){
-        return this.each(function(){
+
+    $.fn.horizonry = function (options) {
+        return this.each(function () {
             (new $.horizonry(this, options));
         });
     };
-    
+
 })(jQuery);
